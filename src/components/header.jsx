0a9ea53152c8777a11c0591b333099c3331945b1
RAUSHAN <mcstar6262@gmail.com>
2025-09-01 22:31:29 +0530

    Created the base Header

---
+++ src/components/header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header()
+{
+    console.log("header rendered");
+    return(
+    <div>
+        <h1>
+            Placement Prep Portal
+        </h1>
+        <p>
+            where experience inspires and teaches others
+        </p>
+    </div>
+    );
+}
+
+export default Header;
